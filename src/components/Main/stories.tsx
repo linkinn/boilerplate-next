52e5cfdb9d55d1df951701f29d7468df252326ec
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2021-02-04 13:50:28 -0300

    create boilerplate

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Title Default',
+    description: 'Description Default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Title Basic',
+  description: 'Description Basic'
+}
+
+export const Default: Story = (args) => <Main {...args} />
